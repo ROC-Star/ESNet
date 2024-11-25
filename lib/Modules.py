fa6e33fcfc890180dba35af0ed8c3d51b4aa6d8c
rp <ren13165238348@163.com>
2024-11-25 10:17:12 +0800

    modify

--- lib/Modules.py
+++ lib/Modules.py
@@ -230,9 +230,9 @@ class SkeletonEncoder(nn.Module):
         super(SkeletonEncoder, self).__init__()
         self.conv1 = _make_layer(BasicBlock, 3, 64, blocks=1, t=3, stride=2)
         self.conv2 = _make_layer(BasicBlock, 64, 64, blocks=2, t=2, stride=2)
-        self.sc1 = ScConv(64)
+        # self.sc1 = ScConv(64)
         self.conv3 = _make_layer(BasicBlock, 64, 32, blocks=2, t=2, stride=2)
-        self.sc2 = ScConv(32)
+        # self.sc2 = ScConv(32)
 
         self.conv_out = nn.Conv2d(32, 1, kernel_size=1, stride=1, padding=0)
         self.conv192_64 = ConvBR(96, 64, 1, 1, 0)
@@ -242,10 +242,10 @@ class SkeletonEncoder(nn.Module):
         feat = self.conv1(x)
 
         feat1 = self.conv2(feat)
-        feat1 = self.sc1(feat1)
+        # feat1 = self.sc1(feat1)
 
         xg = self.conv3(feat1)
-        xg = self.sc2(xg)
+        # xg = self.sc2(xg)
 
         xg_up = F.interpolate(xg, size=feat1.size()[2:], mode='bilinear', align_corners=True)
         xg_f = torch.cat([xg_up, feat1], dim=1)
@@ -658,119 +658,84 @@ class PORM(nn.Module):
         return out1, out2, out3, pose
 
 
-class SCConv(nn.Module):
-    def __init__(self, inplanes, planes, stride, padding, dilation, groups, pooling_r, norm_layer):
-        super(SCConv, self).__init__()
-        self.k2 = nn.Sequential(
-            nn.AvgPool2d(kernel_size=pooling_r, stride=pooling_r),
-            nn.Conv2d(inplanes, planes, kernel_size=3, stride=1,
-                      padding=padding, dilation=dilation,
-                      groups=groups, bias=False),
-            norm_layer(planes),
-        )
-        self.k3 = nn.Sequential(
-            nn.Conv2d(inplanes, planes, kernel_size=3, stride=1,
-                      padding=padding, dilation=dilation,
-                      groups=groups, bias=False),
-            norm_layer(planes),
-        )
-        self.k4 = nn.Sequential(
-            nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride,
-                      padding=padding, dilation=dilation,
-                      groups=groups, bias=False),
-            norm_layer(planes),
-        )
-
-    def forward(self, x):
-        identity = x
-
-        out = torch.sigmoid(
-            torch.add(identity, F.interpolate(self.k2(x), identity.size()[2:])))  # sigmoid(identity + k2)
-        out = torch.mul(self.k3(x), out)  # k3 * sigmoid(identity + k2)
-        out = self.k4(out)  # k4
-
-        return out
-
-
-class SCBottleneck(nn.Module):
-    """SCNet SCBottleneck
-    """
-    expansion = 1
-    pooling_r = 4  # down-sampling rate of the avg pooling layer in the K3 path of SC-Conv.
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None,
-                 cardinality=1, bottleneck_width=32,
-                 avd=False, dilation=1, is_first=False,
-                 norm_layer=None):
-        super(SCBottleneck, self).__init__()
-        group_width = int(planes * (bottleneck_width / 64.)) * cardinality
-        self.conv1_a = nn.Conv2d(inplanes, group_width, kernel_size=1, bias=False)
-        self.bn1_a = norm_layer(group_width)
-        self.conv1_b = nn.Conv2d(inplanes, group_width, kernel_size=1, bias=False)
-        self.bn1_b = norm_layer(group_width)
-        self.avd = avd and (stride > 1 or is_first)
-
-        if self.avd:
-            self.avd_layer = nn.AvgPool2d(3, stride, padding=1)
-            stride = 1
-
-        self.k1 = nn.Sequential(
-            nn.Conv2d(
-                group_width, group_width, kernel_size=3, stride=stride,
-                padding=dilation, dilation=dilation,
-                groups=cardinality, bias=False),
-            norm_layer(group_width),
-        )
-
-        self.scconv = SCConv(
-            group_width, group_width, stride=stride,
-            padding=dilation, dilation=dilation,
-            groups=cardinality, pooling_r=self.pooling_r, norm_layer=norm_layer)
-
-        self.conv3 = nn.Conv2d(
-            group_width * 2, planes, kernel_size=1, bias=False)
-        self.bn3 = norm_layer(planes)
-
-        self.relu = nn.ReLU(inplace=True)
-        self.conv = nn.Sequential(
-            nn.Conv2d(planes, planes, kernel_size=3, stride=2, padding=1),
-            nn.BatchNorm2d(planes),
-            nn.ReLU(inplace=True)
-        )
-        self.downsample = downsample
-        self.dilation = dilation
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out_a = self.conv1_a(x)
-        out_a = self.bn1_a(out_a)
-        out_b = self.conv1_b(x)
-        out_b = self.bn1_b(out_b)
-        out_a = self.relu(out_a)
-        out_b = self.relu(out_b)
-
-        out_a = self.k1(out_a)
-        out_b = self.scconv(out_b)
-        out_a = self.relu(out_a)
-        out_b = self.relu(out_b)
-
-        if self.avd:
-            out_a = self.avd_layer(out_a)
-            out_b = self.avd_layer(out_b)
-
-        out = self.conv3(torch.cat([out_a, out_b], dim=1))
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-        out = self.conv(out)
-
-        return out
+# class SCBottleneck(nn.Module):
+#
+#     expansion = 1
+#     pooling_r = 4  # down-sampling rate of the avg pooling layer in the K3 path of SC-Conv.
+#
+#     def __init__(self, inplanes, planes, stride=1, downsample=None,
+#                  cardinality=1, bottleneck_width=32,
+#                  avd=False, dilation=1, is_first=False,
+#                  norm_layer=None):
+#         super(SCBottleneck, self).__init__()
+#         group_width = int(planes * (bottleneck_width / 64.)) * cardinality
+#         self.conv1_a = nn.Conv2d(inplanes, group_width, kernel_size=1, bias=False)
+#         self.bn1_a = norm_layer(group_width)
+#         self.conv1_b = nn.Conv2d(inplanes, group_width, kernel_size=1, bias=False)
+#         self.bn1_b = norm_layer(group_width)
+#         self.avd = avd and (stride > 1 or is_first)
+#
+#         if self.avd:
+#             self.avd_layer = nn.AvgPool2d(3, stride, padding=1)
+#             stride = 1
+#
+#         self.k1 = nn.Sequential(
+#             nn.Conv2d(
+#                 group_width, group_width, kernel_size=3, stride=stride,
+#                 padding=dilation, dilation=dilation,
+#                 groups=cardinality, bias=False),
+#             norm_layer(group_width),
+#         )
+#
+#         self.scconv = SCConv(
+#             group_width, group_width, stride=stride,
+#             padding=dilation, dilation=dilation,
+#             groups=cardinality, pooling_r=self.pooling_r, norm_layer=norm_layer)
+#
+#         self.conv3 = nn.Conv2d(
+#             group_width * 2, planes, kernel_size=1, bias=False)
+#         self.bn3 = norm_layer(planes)
+#
+#         self.relu = nn.ReLU(inplace=True)
+#         self.conv = nn.Sequential(
+#             nn.Conv2d(planes, planes, kernel_size=3, stride=2, padding=1),
+#             nn.BatchNorm2d(planes),
+#             nn.ReLU(inplace=True)
+#         )
+#         self.downsample = downsample
+#         self.dilation = dilation
+#         self.stride = stride
+#
+#     def forward(self, x):
+#         residual = x
+#
+#         out_a = self.conv1_a(x)
+#         out_a = self.bn1_a(out_a)
+#         out_b = self.conv1_b(x)
+#         out_b = self.bn1_b(out_b)
+#         out_a = self.relu(out_a)
+#         out_b = self.relu(out_b)
+#
+#         out_a = self.k1(out_a)
+#         out_b = self.scconv(out_b)
+#         out_a = self.relu(out_a)
+#         out_b = self.relu(out_b)
+#
+#         if self.avd:
+#             out_a = self.avd_layer(out_a)
+#             out_b = self.avd_layer(out_b)
+#
+#         out = self.conv3(torch.cat([out_a, out_b], dim=1))
+#         out = self.bn3(out)
+#
+#         if self.downsample is not None:
+#             residual = self.downsample(x)
+#
+#         out += residual
+#         out = self.relu(out)
+#         out = self.conv(out)
+#
+#         return out
 
 
 class GroupBatchnorm2d(nn.Module):
